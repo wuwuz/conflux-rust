4a5557bea087364133eaa19ee63061843b0dd0a3
Mingxun Zhou <zhoumingxun@pku.edu.cn>
2021-01-17 19:16:30 +0800

    panic test

--- src/main.rs
+++ src/main.rs
@@ -53,6 +53,7 @@ fn main() -> Result<(), String> {
         });
     } // only for #[cfg]
 
+    panic!("panic in main.rs");
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).version(crate_version!()).get_matches();
 
